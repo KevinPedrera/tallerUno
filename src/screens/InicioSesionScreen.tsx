4377a3cdcf1475b8764654284ba8cee3a9ef6124
KevinPedrera <kevinpedrera@outlook.es>
2025-08-01 03:39:21 -0500

    registro

--- src/screens/InicioSesionScreen.tsx
+++ src/screens/InicioSesionScreen.tsx
@@ -5,19 +5,26 @@ export const InicioSesionScreen = () => {
   return (
     <View>
         <Text> Iniciar Sessión</Text>
+
         <TextInput
-        placeholder='Correo'
+        placeholder='Correo Electrónico'
         placeholderTextColor="#a16207"
         autoCorrect = {false}
         keyboardType='email-address'>
         </TextInput>
+
         <TextInput
         placeholder='Contraseña'
         placeholderTextColor="#a16207"
         secureTextEntry>
         </TextInput>
+
         <TouchableOpacity>
             <Text>Ingresar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+            <Text>No tienes cuenta? Registratre</Text>
         </TouchableOpacity>
     </View>
   )
---
+++ src/screens/RegistroScreen.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+
+export const RegistroScreen = () => {
+  return (
+    <View>
+        <Text>Crear Cuenta</Text>
+        
+        <TextInput
+        placeholder='Nombre'
+        placeholderTextColor="#a16207">
+        </TextInput>
+
+        <TextInput
+        placeholder='Apellido'
+        placeholderTextColor="#a16207">
+        </TextInput>
+
+        <TextInput
+        placeholder='Direccion'
+        placeholderTextColor="#a16207">
+        </TextInput>
+
+        <TextInput
+        placeholder='Telefono'
+        placeholderTextColor="#a16207">
+        </TextInput>
+
+        <TextInput
+        placeholder='Correo Electrónico '
+        placeholderTextColor="#a16207">
+        </TextInput>
+
+        <TextInput
+        placeholder='Contraseña'
+        placeholderTextColor="#a16207">
+        </TextInput>
+
+        <TextInput
+        placeholder='Confirmar La Contraseña'
+        placeholderTextColor="#a16207">
+        </TextInput>
+
+        <TouchableOpacity>
+            <Text>Registrarme</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+            <Text>Ya tienes una cuenta? Inicia sesion</Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
